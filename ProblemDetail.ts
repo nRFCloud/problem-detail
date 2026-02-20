f3c1a2a08c26ffb817b4521b7477ec0152c6cc13
Markus Tacker <Markus.Tacker@NordicSemi.no>
2026-02-20 10:38:07 +0100

    fix: type everything

--- ProblemDetail.ts
+++ ProblemDetail.ts
@@ -1,27 +1,30 @@
-import { Type, type Static, type TObject } from '@sinclair/typebox'
-import { StatusCode } from './HttpStatusCode.ts'
+import {
+	Type,
+	type Static,
+	type TEnum,
+	type TLiteral,
+	type TObject,
+	type TOptional,
+	type TString,
+} from '@sinclair/typebox'
+import { HttpStatusCode, StatusCode } from './HttpStatusCode.ts'
 
 const Context =
 	'https://datatracker.ietf.org/doc/draft-ietf-httpapi-rfc7807bis/'
-
-/**
- * Can be used to throw an error with a Problem Detail object.
- */
-export class ProblemDetailError extends Error {
-	public readonly problem: Static<typeof ProblemDetail>
-	constructor(problem: Omit<Static<typeof ProblemDetail>, '@context'>) {
-		super(problem.title)
-		this.problem = { '@context': Context, ...problem }
-		this.name = 'ProblemDetailError'
-	}
-}
 
 /**
  * Problem Details Object
  *
  * @see https://datatracker.ietf.org/doc/draft-ietf-httpapi-rfc7807bis/
  */
-export const ProblemDetail: TObject = Type.Object(
+export const ProblemDetail: TObject<{
+	'@context': TLiteral<typeof Context>
+	'@id': TOptional<TString>
+	type: TOptional<TString>
+	status: TOptional<TEnum<typeof HttpStatusCode>>
+	title: TString
+	detail: TOptional<TString>
+}> = Type.Object(
 	{
 		'@context': Type.Literal(Context),
 		'@id': Type.Optional(Type.String()),
@@ -36,3 +39,15 @@ export const ProblemDetail: TObject = Type.Object(
 			'See see https://datatracker.ietf.org/doc/draft-ietf-httpapi-rfc7807bis/',
 	},
 )
+
+/**
+ * Can be used to throw an error with a Problem Detail object.
+ */
+export class ProblemDetailError extends Error {
+	public readonly problem: Static<typeof ProblemDetail>
+	constructor(problem: Omit<Static<typeof ProblemDetail>, '@context'>) {
+		super(problem.title)
+		this.problem = { '@context': Context, ...problem }
+		this.name = 'ProblemDetailError'
+	}
+}
